c83b4982eb4e84ce9098fa4d08a1bc377872478d
Paween Theanvanichpant <paween.the@gmail.com>
2017-03-18 20:50:56 +0700

    Basic setup mp3_downloader.py

---
+++ scripts/mp3_downloader.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import sys
+
+spotifyURL = 'https://api.spotify.com/v1/tracks/'
+
+def full_url(track_id):
+    return spotifyURL + track_id
+
+def download_us_mp3():
+    with open('us-chart.json', 'r') as f:
+        data = json.load(f)
+        chart = data['us-chart']
+        for song in chart:
+            url = full_url(song['spotifyID'])
+            r = requests.get(url).json()
+            print r
+            break
+
+def main():
+    argc = len(sys.argv)
+    if argc == 1:
+        print "Please specify regions"
+        print "US, TH, JP"
+        sys.exit(0)
+    else:
+        region = sys.argv[1]
+        if region.lower() == 'us':
+            download_us_mp3()
+        else:
+            raise NotImplementedError
+
+if __name__ == '__main__':
+    main()
