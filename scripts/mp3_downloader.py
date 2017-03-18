cb435bf7250896899b37cc6fb1cfca8944741ee2
Paween Theanvanichpant <paween.the@gmail.com>
2017-03-18 21:10:58 +0700

    Handling no preview_url

--- scripts/mp3_downloader.py
+++ scripts/mp3_downloader.py
@@ -8,17 +8,28 @@ def full_url(track_id):
     return spotifyURL + track_id
 
 def download_us_mp3():
+    error = 0
     with open('us-chart.json', 'r') as f:
         data = json.load(f)
         chart = data['us-chart']
         for song in chart:
             url = full_url(song['spotifyID'])
             r = requests.get(url).json()
-            preview_url = r['preview_url']
-            song_title = song['title'].split(' ')
-            file_name = "-".join(song_title) + '.mp3'
-            print file_name
-
+            try:
+                preview_url = r['preview_url']
+                print "-"*50
+                print "has preview_url"
+                song_title = song['title'].split(' ')
+                file_name = "-".join(song_title) + '.mp3'
+                print file_name
+            except Exception as e:
+                error += 1
+                print "-"*50
+                print "no preview_url"
+                song_title = song['title'].split(' ')
+                file_name = "-".join(song_title) + '.mp3'
+                print file_name
+        print "No. of no preview_url:", error
 def main():
     argc = len(sys.argv)
     if argc == 1:
