d4d55a51b43e2b8401ef2aee16a398127395d521
Paween Theanvanichpant <paween.the@gmail.com>
2017-03-18 21:43:56 +0700

    Save us_spotify_responses.json

--- scripts/mp3_downloader.py
+++ scripts/mp3_downloader.py
@@ -1,3 +1,4 @@
+import arrow
 import json
 import requests
 import sys
@@ -6,6 +7,22 @@ spotifyURL = 'https://api.spotify.com/v1/tracks/'
 
 def full_url(track_id):
     return spotifyURL + track_id
+
+def save_spotify_responses():
+    with open('us-chart.json', 'r') as f:
+        data = json.load(f)
+        chart = data['us-chart']
+        responses = []
+        time = arrow.now().isoformat()
+        for song in chart:
+            url = full_url(song['spotifyID'])
+            r = requests.get(url).json()
+            responses.append(r)
+    with open('us_spotify_responses.json', 'w') as f:
+        output = {}
+        output['data'] = responses
+        output['date-retrieved'] = time
+        json.dump(output, f, indent=2)
 
 def download_us_mp3():
     error = 0
@@ -17,11 +34,11 @@ def download_us_mp3():
             r = requests.get(url).json()
             try:
                 preview_url = r['preview_url']
-                print "-"*50
-                print "has preview_url"
-                song_title = song['title'].split(' ')
-                file_name = "-".join(song_title) + '.mp3'
-                print file_name
+                # print "-"*50
+                # print "has preview_url"
+                # song_title = song['title'].split(' ')
+                # file_name = "-".join(song_title) + '.mp3'
+                # print file_name
             except Exception as e:
                 error += 1
                 print "-"*50
@@ -30,6 +47,7 @@ def download_us_mp3():
                 file_name = "-".join(song_title) + '.mp3'
                 print file_name
         print "No. of no preview_url:", error
+
 def main():
     argc = len(sys.argv)
     if argc == 1:
