3d44665fbf3279cf141703e05547ce5c9fe90b1d
Paween Theanvanichpant <paween.the@gmail.com>
2017-03-19 17:46:51 +0700

    Download UK tracks

--- scripts/mp3_downloader.py
+++ scripts/mp3_downloader.py
@@ -29,6 +29,32 @@ def save_spotify_responses(csv_file, output_file):
     write_to_json(output, output_file)
 
 
+def download_uk_mp3():
+    data = read_from_json('uk_spotify_responses.json')
+    songs = data['data']
+    n = 100
+    has_no_preview_url = 0
+    for song in songs[:n]:
+        preview_url = song['preview_url']
+        if preview_url is None:
+            print song['name']
+            has_no_preview_url += 1
+            continue
+        else:
+            file_name = '-'.join(song['name'].split(' ')) + '.mp3'
+            file_path = "tracks/uk/"
+            full_path = file_path + file_name
+            r = requests.get(preview_url, stream=True)
+            # Taken from
+            # http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+            with open(full_path, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+                        # f.flush() commented by recommendation from J.F.Sebastian
+    print "Total songs that has no preview_url:", has_no_preview_url
+
+
 def download_us_mp3():
     data = read_from_json('us_spotify_responses.json')
     songs = data['data']
@@ -65,6 +91,8 @@ def main():
         region = sys.argv[1]
         if region.lower() == 'us':
             download_us_mp3()
+        elif region.lower() == 'uk':
+            download_uk_mp3()
         else:
             raise NotImplementedError
 
